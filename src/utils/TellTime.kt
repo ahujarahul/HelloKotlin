f06737c3815a810a3208233eb5a44f4182211574
ahujarahul <ahuja.rahul05@gmail.com>
2017-10-04 19:19:12 +0530

    1) comments added how to use conditions as expressions

--- src/utils/TellTime.kt
+++ src/utils/TellTime.kt
@@ -27,21 +27,21 @@ class TellTime {
     }
 
 
-    /* when as expression
+    /* when as expression -- returned string will be stored in 'return'
     *
     * var return = when(hour){
           0, 1, 2, 3, 4 -> {
               //multiple lines in a case
-              println("it is midnight")
-              println("Do not disturb me")
+              "it is midnight"
+              "Do not disturb me"
           }// case ends
 
-          5 -> println("good morning")
-          12 -> println("good noon")
-          16 -> println("good evening")
-          22 -> println("good night")
+          5 -> "good morning"
+          12 -> "good noon"
+          16 -> "good evening"
+          22 -> "good night"
 
-          else -> println("DND i am at work")
+          else -> "DND i am at work"
       }
     */
 }
